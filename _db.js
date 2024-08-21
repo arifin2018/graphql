75a535f9dd25b4c9258fcbe1d5bad66782b0c508
arifin2018 <arifingdr@gmail.com>
2024-08-22 01:50:38 +0700

    update index.js

--- _db.js
+++ _db.js
@@ -1,16 +1,34 @@
 const books = [
-    {ID:1, name: "surya dharma", release: 2001 },
-    {ID:2, name: "The Awakening", release: 1899 },
-    {ID:3, name: "City of Glass", release: 1994 },
-    {ID:4, name: "The Great Gatsby", release: 1925 }
+    {ID:1, name: "surya dharma2", release: 2001, authors_id:1 },
+    {ID:2, name: "The Awakening2", release: 1899, authors_id:2 },
+    {ID:3, name: "City of Glas2s", release: 1994, authors_id:3 },
+    {ID:4, name: "The Great Gatsb2y", release: 1925, authors_id:4 }
 ]
 
 const authors = [
-    {name: "surya dharma"},
-    {name: "The Awakenings"},
-    {name: "City of Glass"},
-    {name: "The Great Gatsby"},
-    {name: "F. Scott Fitzgerald"}
+    {ID:1,name: "surya dharma"},
+    {ID:2,name: "The Awakenings"},
+    {ID:3,name: "City of Glass"},
+    {ID:4,name: "The Great Gatsby"},
+    {ID:5,name: "F. Scott Fitzgerald"}
 ]
 
-export {books,authors}
+const lending = [
+    {
+        ID:1,
+        book_id: 1,
+        user_id:1
+    },
+    {
+        ID:2,
+        book_id: 3,
+        user_id:2
+    },
+    {
+        ID:3,
+        book_id: 3,
+        user_id:1
+    }
+]
+
+export {books,authors,lending}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { authors, books } from "./_db.js";
+import { authors, books, lending } from "./_db.js";
 
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
@@ -10,7 +10,8 @@ const typeDefs = `#graphql
     ID: ID
     name: String
     release: String
-    author: Author!
+    author: Author!,
+    lending: [Lending]
   }
 
   type User{
@@ -23,6 +24,12 @@ const typeDefs = `#graphql
     name: String
   }
 
+  type Lending{
+    ID: ID
+    book: [Book]
+    user: User
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
@@ -31,17 +38,20 @@ const typeDefs = `#graphql
     book(id: ID!): Book!
     users: [User]
     authors: [Author]
+    lending: [Lending]
   }
 `;
 
 
 const users = [
     {
+      ID:1,
       name: 'The Awakening',
       age: 22,
       address: 'Kate Chopin',
     },
     {
+      ID:2,
       name: 'City of Glass',
       age: 3,
       address: 'Paul Auster',
@@ -58,7 +68,32 @@ const resolvers = {
       },
       authors: () => authors,
       users: () => users,
+      lending: () => lending
     },
+    Lending:{
+      book(parent) {
+        return books.filter((book) => {
+          return book.ID == parent.book_id
+        });
+      },
+      user(parent) {
+        return users.find((user) => {
+          return user.ID == parent.user_id
+        });
+      },
+    },
+    Book:{
+      author(parent) {
+        return authors.find((author) => {
+          return author.ID == parent.authors_id
+        });
+      },
+      lending(parent) {
+        return lending.filter((lending) => {
+          return lending.book_id == parent.ID
+        });
+      },
+    }
 };
 
 const server = new ApolloServer({
